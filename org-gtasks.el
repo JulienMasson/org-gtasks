5bbaadaa58cc36952d544e9e0a82cdad87bccc5e
Julien Masson <jmasson@baylibre.com>
2019-12-05 11:11:31 +0100

    Add GPL license and update org-gtask header
    Fixes #3
    
    Signed-off-by: Julien Masson <massonju.eseo@gmail.com>

--- org-gtasks.el
+++ org-gtasks.el
@@ -1,21 +1,22 @@
-;;; org-gtasks.el
+;;; org-gtasks.el -- Export/import all Google Tasks to org files.
 
-;; Copyright (C) 2018 Julien Masson
+;; Copyright (C) 2018-2019 Julien Masson
 
-;; This file is NOT part of GNU Emacs.
+;; Author: Julien Masson <massonju.eseo@gmail.com>
+;; URL: https://github.com/JulienMasson/org-gtasks
 
-;; GNU Emacs is free software: you can redistribute it and/or modify
+;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 (require 'json)
 (require 'request)
